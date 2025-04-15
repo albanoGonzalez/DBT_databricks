bc6ed02b427d84ccd62c55f6f77fe60bd16a8e62
albano.gonzalez@allyourbi.nl <albano.gonzalez@allyourbi.nl>
2025-04-15 09:36:16 +0000

    adding my first macro and implementing some jinja code

---
+++ models/marts/finance/int_orders__pivoted.sql
@@ -0,0 +1,24 @@
+{%- set paymentmethod = ['bank_transfer','coupon', 'credit_card','gift_card'] -%}
+
+with payments as (
+    select * from {{ ref('stg_stripe_payments') }}
+),
+
+pivoted as (
+    select 
+    order_id,
+
+    {%- for method in paymentmethod %}
+        sum(case when method='{{ method }}' then amount else 0 end) as {{ method }}_amount
+        {%- if not loop.last -%}
+            ,
+        {%- endif %}
+
+    {%- endfor %}
+
+    from payments
+    where status = 'success'
+    group by 1
+)
+
+select * from pivoted
