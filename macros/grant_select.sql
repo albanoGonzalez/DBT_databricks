d14cc5e1ba349166fe4c0c1b67195ae8f08d0a77
albano.gonzalez@allyourbi.nl <albano.gonzalez@allyourbi.nl>
2025-04-16 08:35:41 +0000

    grant select macros

---
+++ macros/grant_select.sql
@@ -0,0 +1,11 @@
+{% macro grant_select(schema=target.schema, role=target.role) %}
+
+  {% set sql %}
+    grant usage on schema default to {{role}}
+  {% endset %}
+
+  {{ log('Granting select on all tables and views in schema ' ~ target.schema ~ ' to role ' ~ role, info=True) }}
+  {% do run_query(sql) %}
+  {{ log('Privileges granted', info=True) }}
+
+{% endmacro %}
